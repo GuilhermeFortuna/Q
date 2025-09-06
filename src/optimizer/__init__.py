c2dcef2b34ef43b7c38f18b4aaece64b3cd17303
Guilherme Fortuna <guilherme.fortuna@getjones.com>
2025-09-06 03:20:36 -0300

    Add `optimizer` package: integrate Optuna for strategy optimization; update dependencies and workspace configuration

---
+++ src/optimizer/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.0"
+__description__ = "A package for strategy optimization using Optuna."
+
+
+def get_package_info():
+    """Return package information as a dictionary."""
+    return {
+        'name': 'optimizer',
+        'version': __version__,
+        'description': __description__,
+    }
